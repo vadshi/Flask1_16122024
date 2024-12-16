69793e74aacba7ea633ce6a41dc5ab19094438bd
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-12-16 14:52:25 +0300

    Add jsonify examples

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -12,12 +13,12 @@ about_me = {
 
 @app.route("/") # Это первый URL, который мы будем обрабатывать
 def hello_world():  # Это функция-обработчик, которая будет вызвана для обработки URL'a
-   return "Hello, World!"
+   return jsonify(hello="Hello, World!"), 200
 
 
 @app.route("/about")
 def about():
-   return about_me
+   return jsonify(about_me), 200
 
 
 if __name__ == "__main__":
