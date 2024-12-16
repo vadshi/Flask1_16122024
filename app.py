5f45a3554d44cac87a24522bd4acb3dfd3f040e3
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-12-16 15:12:03 +0300

    Add quotes URL

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from typing import Any
 
 
 app = Flask(__name__)
@@ -11,15 +12,50 @@ about_me = {
 }
 
 
+quotes = [
+   {
+       "id": 3,
+       "author": "Rick Cook",
+       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с \
+         большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. \
+         Пока вселенная побеждает."
+   },
+   {
+       "id": 5,
+       "author": "Waldi Ravens",
+       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+   },
+   {
+       "id": 6,
+       "author": "Mosher's Law of Software Engineering",
+       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+   },
+   {
+       "id": 8,
+       "author": "Yoggi Berra",
+       "text": "В теории, теория и практика неразделимы. На практике это не так."
+   },
+
+]
+
+
 @app.route("/") # Это первый URL, который мы будем обрабатывать
 def hello_world():  # Это функция-обработчик, которая будет вызвана для обработки URL'a
-   return jsonify(hello="Hello, World!"), 200
+    return jsonify(hello="Hello, World!"), 200
 
 
 @app.route("/about")
 def about():
-   return jsonify(about_me), 200
+    return jsonify(about_me), 200
+
+
+# URL: /quotes
+@app.route("/quotes")
+def get_quotes() -> list[dict[str: Any]]:
+    """ Функция преобразует список словарей в массив объектов JSON."""
+    return jsonify(quotes), 200
+   
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
